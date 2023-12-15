b59478c0a31c6285a022109c0b9411a0172a8e1e
Kirk Charles Niverba <kirk@moveup.app>
2023-12-15 15:32:08 +0800

    chore: clean

--- lib/pages/screens/home_page.dart
+++ lib/pages/screens/home_page.dart
@@ -7,7 +7,6 @@ import 'package:dashed_circular_progress_bar/dashed_circular_progress_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animation_progress_bar/flutter_animation_progress_bar.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
